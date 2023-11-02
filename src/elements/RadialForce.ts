21637fe8a9aa247341711089e012e7e0c800c791
AnasAsad <anas.asad@letsmod.com>
2023-11-02 18:21:19 +0200

    Implemented Radial Force

---
+++ src/elements/RadialForce.ts
@@ -0,0 +1,36 @@
+import { BodyHandle } from "../engine/BodyHandle";
+import { GameplayScene } from "../engine/GameplayScene";
+import { LMent } from "../engine/LMent";
+import { CollisionHandler, CollisionInfo } from "../engine/MessageHandlers";
+import { global, js_new } from "../js";
+
+export class RadialForce extends LMent implements CollisionHandler
+{
+    forceValue: number;
+
+    constructor(body: BodyHandle, id: number, params: Partial<RadialForce> = {}) {
+        super(body, id, params);
+        this.forceValue = params.forceValue === undefined ? 1 : params.forceValue;
+    }
+
+    onInit(): void {
+        GameplayScene.instance.dispatcher.addListener("collision", this);
+    }
+
+    onStart(): void {
+    }
+
+    onCollision(info: CollisionInfo): void {
+        let other = GameplayScene.instance.getBodyById(info.getOtherObjectId());
+        if (other !== undefined) {
+            let directionFromCenter = js_new(global.THREE.Vector3, 0, 0, 0).subVectors(other.body.getPosition(), this.body.body.getPosition()).normalize();
+            let distanceFromCenter = other.body.getPosition().distanceTo(this.body.body.getPosition());
+            let forceMagnitude = this.forceValue / distanceFromCenter;
+            if (forceMagnitude > this.forceValue) {
+                forceMagnitude = this.forceValue;
+            }
+            other.body.applyCentralForce(directionFromCenter.multiplyScalar(forceMagnitude));
+        }
+    }
+    
+}
