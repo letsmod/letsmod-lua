9a907d8590da9767f395f18ab3d4ee5cf31eb9e3
AnasAsad <anas.asad@letsmod.com>
2023-11-13 20:16:27 +0200

    -Changed TTL to AutoDestroy and edited the code to work with giving it bodies names -Changed TTH to AutoHide and edited the code to work with giving it bodies names

--- src/elements/_ElementTypes.ts
+++ src/elements/_ElementTypes.ts
@@ -29,5 +29,5 @@ export { ColorFlicker } from "./ColorFlicker";
 export { LockRotationAxis } from "./LockRotationAxis";
 export { ContactForce } from "./ContactForce";
 export { ContactKnockback } from "./ContactKnockback";
-export {TTL} from "./TTL";
-export {TTH} from "./TTH";
+export {AutoDestroy} from "./AutoDestroy";
+export {AutoHide} from "./AutoHide";
