42c12082226d142f930c7e1c3c58948d84beb3fb
letsAhmad <ahmad@letsmod.com>
2023-12-06 12:25:05 +0200

    Migration
    Migrating my work from laptop to desktop. This needs more work.

--- src/elements/Waypoint.ts
+++ src/elements/Waypoint.ts
@@ -16,10 +16,9 @@ export class Waypoint extends LMent implements PhysicsSubstepHandler {
     points: waypoints[];
     pattern: string;
     private totalDistanceToNextPoint: number;
-    private index: number;
+    private nextIndex: number;
     private delayPlusTime: number;
     private now: number;
-    private timeFunctionStart: number;
     private indexAddvalue: number;
 
     constructor(body: BodyHandle, id: number, params: Partial<Waypoint> = {}) {
@@ -28,87 +27,131 @@ export class Waypoint extends LMent implements PhysicsSubstepHandler {
         this.points = this.convertArray(params.points) || [];
         this.pattern = params.pattern === undefined ? "loop" : params.pattern;
         this.totalDistanceToNextPoint = 0;
-        this.index = 0;
+        this.nextIndex = 0;
         this.delayPlusTime = 0;
         this.now = 0;
-        this.timeFunctionStart = 0;
         this.indexAddvalue = 1;
     }
 
     onInit(): void {
         GameplayScene.instance.dispatcher.addListener("physicsSubstep", this);
+        this.validatePattern();
     }
 
     onStart(): void {
+        this.adjustOffsets();
+        this.adjustPatterns();
+
+        this.totalDistanceToNextPoint = this.body.body.getPosition().distanceTo(this.points[0].offset);
+    }
+
+    adjustPatterns() {
+        const InitWayPoint: waypoints = {
+            offset: this.body.body.getPosition().clone(),
+            speed: this.points[0].speed,
+            delay: this.points[0].delay,
+            interpolationFunction: this.points[0].interpolationFunction
+        };
+
+        if (this.pattern === "pingpong") {
+            this.points.unshift(InitWayPoint);
+            this.indexAddvalue = -1;
+        } else if (this.pattern === "loop")
+            this.points.push(InitWayPoint);
+    }
+
+    adjustOffsets() {
         for (let i = 0; i < this.points.length; i++) {
             let offset = Helpers.NewVector3(this.points[i].offset.x, this.points[i].offset.y, this.points[i].offset.z);
             offset.applyQuaternion(this.body.body.getRotation());
-            if (i == 0) {
-                this.points[i].offset = Helpers.NewVector3(
-                    this.body.body.getPosition().x + offset.x, this.body.body.getPosition().y + offset.y, this.body.body.getPosition().z + offset.z);
-            } else {
-                this.points[i].offset = Helpers.NewVector3(
-                    this.points[i - 1].offset.x + offset.x, this.points[i - 1].offset.y + offset.y, this.points[i - 1].offset.z + offset.z);
-            }
+            if (i == 0)
+                this.points[i].offset = this.body.body.getPosition().clone().add(offset);
+            else this.points[i].offset = this.points[i-1].offset.clone().add(offset);
         }
-        if (this.pattern !== "once") {
-            let InitWayPoint: waypoints = { offset: this.body.body.getPosition().clone(), speed: this.points[0].speed, delay: this.points[0].delay, interpolationFunction: this.points[0].interpolationFunction };
-            if (this.pattern === "pingpong") {
-                this.points.unshift(InitWayPoint);
-                this.indexAddvalue = -1;
-            }
-            else if (this.pattern === "loop")
-                this.points.push(InitWayPoint);
+    }
+
+    validatePattern() {
+        if (this.pattern !== "loop" && this.pattern !== "pingpong" && this.pattern !== "once") {
+            console.log("Pattern " + this.pattern + " is not found, resetting to loop");
+            this.pattern = "loop";
         }
-        this.totalDistanceToNextPoint = this.body.body.getPosition().distanceTo(this.points[0].offset);
     }
 
     onPhysicsSubstep(substepDt: number): void {
         this.now = GameplayScene.instance.memory.timeSinceStart;
-        if (this.points[this.index] != undefined) {
-            if (this.now - this.delayPlusTime >= this.points[this.index].delay) {
-                if (this.points[this.index].interpolationFunction === "linear") {
-                    this.linearMovement(this.points[this.index], substepDt);
-                } else if (this.points[this.index].interpolationFunction === "sine") {
-                    this.sineMovement(this.points[this.index], substepDt);
-                } else {
-                    console.log("Wrong interpolationFunction");
-                }
-            }
+        if (this.points[this.nextIndex] != undefined) {
+            if (this.now - this.delayPlusTime >= this.points[this.nextIndex].delay)
+                this.moveOneStep(this.points[this.nextIndex], substepDt);
         }
     }
 
-    linearMovement(target: waypoints, dt: number): void {
-        let direction = Helpers.zeroVector.subVectors(target.offset, this.body.body.getPosition()).normalize();
-        let movement = direction.clone().multiplyScalar(target.speed * dt);
+    moveOneStep(target: waypoints, dt: number): void {
+        const position = this.body.body.getPosition();
+        const distanceToTarget = position.distanceTo(target.offset);
+        const normalizedDistance = distanceToTarget / this.totalDistanceToNextPoint;
+        const moveFactor = this.movementStepFactor(normalizedDistance, target.interpolationFunction);
 
-        this.body.body.setPosition(this.body.body.getPosition().clone().add(movement));
-
-        if (this.body.body.getPosition().clone().distanceTo(target.offset) < target.speed * dt) {
+        if (distanceToTarget < target.speed * dt * moveFactor) {
             this.body.body.setPosition(target.offset);
-            if (this.pattern === "pingpong" && (this.index === 0 || this.index === this.points.length - 1))
-                this.indexAddvalue *= -1;
-            this.index += this.indexAddvalue;
+            this.updateNextIndex();
+        } else {
+            const direction = target.offset.clone().sub(position).normalize();
 
-            while (this.points[this.index] === undefined) {
-                this.index += 1;
-                if (this.index > this.points.length) {
-                    if (this.pattern === "once")
-                        this.index = this.points.length - 1;
-                    else if (this.pattern === "pingpong")
-                        this.index = this.points.length - 2;
-                    else if (this.pattern === "loop")
-                        this.index = 0;
-                    else console.log("Wrong pattern");
-                    break;
-                }
-            }
+            const movement = direction.clone().multiplyScalar(target.speed * dt * moveFactor);
 
-            this.totalDistanceToNextPoint = this.body.body.getPosition().distanceTo(this.points[this.index].offset);
-            this.delayPlusTime = this.now + this.points[this.index].delay;
+            position.add(movement);
+            this.body.body.setPosition(position);
         }
     }
 
-    sineMovement(target: waypoints, dt: number): void {
+    updateNextIndex() {
+        if (this.pattern === "pingpong" && (this.nextIndex === 0 || this.nextIndex === this.points.length - 1)) {
+            this.indexAddvalue *= -1;
+        }
+        this.nextIndex += this.indexAddvalue;
+
+        while (this.nextIndex < 0 || this.nextIndex >= this.points.length) {
+            if (this.pattern.toLowerCase() === "once") {
+                this.nextIndex = this.points.length - 1;
+            } else if (this.pattern.toLowerCase() === "pingpong") {
+                this.nextIndex = this.nextIndex < 0 ? 1 : this.points.length - 2;
+            } else {
+                this.nextIndex = 0; // Default is "loop"
+            }
+        }
+
+        this.totalDistanceToNextPoint = this.body.body.getPosition().distanceTo(this.points[this.nextIndex].offset);
+        this.delayPlusTime = this.now + this.points[this.nextIndex].delay;
+    }
+
+    movementStepFactor(t: number, interpolation: string): number {
+        switch (interpolation) {
+            case "sine":
+                return 0.1 + Math.sin(Math.PI * t) * 0.9;
+            case "smooth":
+                return 0.1 + Helpers.NumLerp(Math.sqrt(t), Math.sqrt(1 - t), t);
+            case "elastic":
+                return 0.1 + this.elasticEase(t);
+            default:
+                return 1;
+        }
+    }
+
+    elasticEase(t: number): number {
+        const amplitude = 1.0;
+        const period = 0.3;
+        const overshoot = 1.70158; // Adjust for desired overshoot behavior
+
+        if (t === 0 || t === 1) {
+            return t;
+        } else {
+            const s = period / (2 * Math.PI) * Math.asin(1);
+            return (
+                amplitude *
+                Math.pow(2, -10 * t) *
+                Math.sin(((t - s) * (2 * Math.PI)) / period) *
+                overshoot
+            );
+        }
     }
 }
