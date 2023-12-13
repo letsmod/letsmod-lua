c6bf6af25a09d810c44381351809a7bb33729942
Noon-u <113030242+Noon-u@users.noreply.github.com>
2023-12-13 21:46:18 +0200

    added dot product to TriggerOnCollision

--- src/elements/TriggerOnCollision.ts
+++ src/elements/TriggerOnCollision.ts
@@ -1,18 +1,24 @@
 import { BodyHandle } from "engine/BodyHandle";
 import { GameplayScene } from "engine/GameplayScene";
+import { Helpers } from "engine/Helpers";
 import { LMent } from "engine/LMent";
 import { CollisionHandler, CollisionInfo } from "engine/MessageHandlers";
+import { Vector3 } from "three";
 
 export class TriggerOnCollision extends LMent implements CollisionHandler {
   triggerId: string | undefined;
   triggerContext: "local" | "group" | "global";
   triggerOnCollisionWithElementType: string | undefined;
-
+  contactDirection: Vector3;
+  dotMinimum: number | undefined;
+  
   constructor(body: BodyHandle, id: number, params: Partial<TriggerOnCollision> = {}) {
     super(body, id, params);
     this.triggerId = params.triggerId;
     this.triggerContext = params.triggerContext === undefined ? "group" : params.triggerContext;
     this.triggerOnCollisionWithElementType = params.triggerOnCollisionWithElementType;
+    this.contactDirection = params.contactDirection === undefined ? Helpers.upVector : params.contactDirection;
+    this.dotMinimum = params.dotMinimum;
   }
 
   onInit() {
@@ -23,16 +29,20 @@ export class TriggerOnCollision extends LMent implements CollisionHandler {
   }
 
   onCollision(info: CollisionInfo) {
-    if (this.triggerOnCollisionWithElementType === undefined) {
-      this.sendTrigger();
-    }
-    else {
-      let other = GameplayScene.instance.getBodyById(info.getOtherObjectId());
-      if (other?.getElementByTypeName(this.triggerOnCollisionWithElementType) !== undefined) {
-        this.sendTrigger();
+    let other = GameplayScene.instance.getBodyById(info.getOtherObjectId());
+    if (other) {
+      let collisionDirection = info.getDeltaVOther().normalize();
+      let adjustedContactDirection = this.contactDirection.clone().applyQuaternion(this.body.body.getRotation());
+
+      let dotProduct = collisionDirection.dot(adjustedContactDirection);
+      if (this.dotMinimum === undefined || dotProduct >= this.dotMinimum) {
+        if (this.triggerOnCollisionWithElementType === undefined || other.getElementByTypeName(this.triggerOnCollisionWithElementType) !== undefined) {
+          this.sendTrigger();
+        }
       }
     }
   }
+
 
   sendTrigger() {
     if (this.triggerId !== undefined) {
