6c74c7b778d74d0b63fd662bdac8a7a38fe9a620
AnasAsad <anas.asad@letsmod.com>
2023-12-19 15:02:52 +0200

    -Bug fix for both to not work with hologram -Added dotminimum for SetEnabledOnCollision

--- src/elements/SetEnabledOnCollision.ts
+++ src/elements/SetEnabledOnCollision.ts
@@ -1,7 +1,9 @@
 import { BodyHandle } from "engine/BodyHandle";
 import { GameplayScene } from "engine/GameplayScene";
+import { Helpers } from "engine/Helpers";
 import { LMent } from "engine/LMent";
 import { CollisionHandler, CollisionInfo } from "engine/MessageHandlers";
+import { Vector3 } from "three";
 
 export class SetEnabledOnCollision extends LMent implements CollisionHandler {
     elementName: string;
@@ -9,6 +11,8 @@ export class SetEnabledOnCollision extends LMent implements CollisionHandler {
     collisionMinDeltaV?: number;
     elementChipName: string;
     setEnabled: boolean;
+    dotMinimum?: number;
+    contactDirection: Vector3;
 
     constructor(body: BodyHandle, id: number, params: Partial<SetEnabledOnCollision> = {}) {
         super(body, id, params);
@@ -17,6 +21,8 @@ export class SetEnabledOnCollision extends LMent implements CollisionHandler {
         this.collisionMinDeltaV = params.collisionMinDeltaV;
         this.collisionMinImpulse = params.collisionMinImpulse;
         this.setEnabled = params.setEnabled ?? true;
+        this.contactDirection = params.contactDirection ?? Helpers.upVector;
+        this.dotMinimum = params.dotMinimum;
     }
 
     onInit(): void {
@@ -36,12 +42,18 @@ export class SetEnabledOnCollision extends LMent implements CollisionHandler {
             if (other !== undefined && i == other)
                 inBodyGroup = true;
         }
-        if (!inBodyGroup && other?.body.name !== "MainCamera_Lua") {
-            if (impulseSufficient && deltaVSufficient || this.body.body.getPhysicsBodyType() === 2) {
-                let elements = this.body.getAllElementsByTypeName(this.elementName);
-                for (let i = 0; i < elements.length; i++) {
-                    if (elements[i].name === this.elementChipName || this.elementChipName === "")
-                        elements[i].enabled = this.setEnabled;
+        if (!inBodyGroup && other && other.body.name !== "MainCamera_Lua" && other.body.getPhysicsBodyType() !== 2) {
+            let collisionDirection = info.getDeltaVOther().normalize();
+            let adjustedContactDirection = this.contactDirection.clone().applyQuaternion(this.body.body.getRotation());
+
+            let dotProduct = collisionDirection.dot(adjustedContactDirection);
+            if (this.dotMinimum === undefined || dotProduct >= this.dotMinimum) {
+                if (impulseSufficient && deltaVSufficient || this.body.body.getPhysicsBodyType() === 2) {
+                    let elements = this.body.getAllElementsByTypeName(this.elementName);
+                    for (let i = 0; i < elements.length; i++) {
+                        if (elements[i].name === this.elementChipName || this.elementChipName === "")
+                            elements[i].enabled = this.setEnabled;
+                    }
                 }
             }
         }
