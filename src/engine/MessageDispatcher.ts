774bf5727318b3b8786da771eebb9624c792ff32
sir-aureus <sir.aureus@gmail.com>
2024-03-21 17:01:08 -0700

    - added new "actorTapped" handler type to message dispatcher

--- src/engine/MessageDispatcher.ts
+++ src/engine/MessageDispatcher.ts
@@ -520,4 +520,16 @@ export class MessageDispatcher
       }  
     }
   }
+
+  onActorTapped(actor: BodyHandle)
+  {
+    let listeners = this.listeners["actorTapped"].slice();
+    for (let i = 0; i < listeners.length; i++)
+    {
+      let listener = listeners[i];
+      if (listener.enabled) {
+        listener.onActorTapped(actor);
+      }
+    }
+  }
 }
