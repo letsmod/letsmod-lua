7512aefeec5b3c19bc9a37d622574372fbf06d32
Noon-u <113030242+Noon-u@users.noreply.github.com>
2024-03-27 15:54:03 +0200

    Hard coded element for Borgir mod

--- src/elements/PlayAnimationOnCollision.ts
+++ src/elements/PlayAnimationOnCollision.ts
@@ -9,11 +9,11 @@ export class PlayAnimationOnCollision extends LMent implements CollisionHandler 
     
     constructor(body: BodyHandle, id: number, params: Partial<PlayAnimationOnCollision>) {
         super(body, id, params);
-        this.targetObjectName = params.targetObjectName ?? ""; // Default to an empty string if not provided
-        this.animationName = params.animationName ?? ""; // Default to an empty string if not provided
+        this.targetObjectName = params.targetObjectName ?? "";
+        this.animationName = params.animationName ?? "";
     }
     onStart(): void {
-        this.body.body.getShapes()[0].playAnimation("temp_idle", 1);
+        this.body.body.getShapes()[0].playAnimation("idle_v3", 1);
     }
     
     onInit(): void {
@@ -23,8 +23,7 @@ export class PlayAnimationOnCollision extends LMent implements CollisionHandler 
     onCollision(info: CollisionInfo): void {
         const other = GameplayScene.instance.getBodyById(info.getOtherObjectId());
         if (other && other.body.name === this.targetObjectName) {
-            // Assuming shapes[0] is the shape you want to target
-            // and playAnimation is a valid method for a shape object
+
             this.body.body.getShapes()[0].playAnimation(this.animationName,1);
 
             console.log("Playing animation " + this.animationName + " on " + this.targetObjectName);
